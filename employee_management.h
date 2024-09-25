e3262b8190d5ff9d61eb07340dff6d9c12c6c541
sherif osama <sherif.os.abdelkareem@gmail.com>
2024-09-26 01:55:11 +0300

    Delete employee_management.h

--- employee_management.h
+++
@@ -1,8 +0,0 @@
-#include "typedef.h"
-void addEmployee (void) ;
-void displayEmployee (void) ;
-sint16_t  findIndexByID (sint16_t search_id ) ;
-void statistics (void ) ;
-void take_avacation (void) ;
-void employeeServices (void) ;
-void display_vacations (void) ;
